6fc6c54afae8cceefd64ce12bb6c32dd9cdf1943
Basti <sebastian.hedwig@web.de>
2025-09-06 19:29:55 +0200

    added templates and rendering functions. added base design elements

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function init(){
-    
+    renderHeaderTemplate();
+    renderPageCOntentSkeletonTemplate();
+    renderFooterTemplate();
+}
+
+function renderHeaderTemplate() {
+    const pageHeadRef = document.getElementById('page_head');
+    pageHeadRef.innerHTML = getHeaderTemplate();
+}
+
+function renderFooterTemplate() {
+    const pageFooterRef = document.getElementById('page_footer');
+    pageFooterRef.innerHTML = getFooterTemplate();
+}
+
+function renderPageCOntentSkeletonTemplate() {
+    const pageContentRef = document.getElementById('page_content');
+    pageContentRef.innerHTML = getPageContentSkeletonTemplate();
 }
---
+++ scripts/templates.js
@@ -0,0 +1,33 @@
+// Main-Templates:
+function getHeaderTemplate() {
+    return /*html*/ `
+        <div id="header_content" class="content_wrapper p-lr-2r">
+            <img class="header_logo" src="./assets/img/Pokedex_Logo_textstudio.png" alt="Pokedex Logo">
+        </div>
+    `;
+}
+
+function getPageContentSkeletonTemplate() {
+    return /*html*/ `
+        <div id="main_content_container" class="content_wrapper p-lr-2r">
+            <div id="pokesearch_container">
+                <img id="pokesearch_title_img" src="./assets/img/Pokesearch_Logo_textstudio.png" alt="Pokesearch Schriftzug">
+                <form id="pokesearch_box">
+                    <input type="search">
+                    <input type="button" value="Search">
+                </form>
+
+            </div>
+            <div id="pokemon_card_box"></div>
+        </div>
+    `;
+}
+
+
+function getFooterTemplate() {
+    return /*html*/ `
+        <div id="footer_content" class="content_wrapper p-lr-2r">
+            <span>© 2025 Sebastian Hedwig</span>
+        </div>         
+    `;
+}
