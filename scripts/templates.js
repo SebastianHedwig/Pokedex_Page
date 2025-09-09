f6e96360820b070d50c701bcf554231b19167a29
Basti <sebastian.hedwig@web.de>
2025-09-10 01:40:32 +0200

    changed css class p-lr-1r

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,14 +1,14 @@
 // Main-Templates:
 function getHeaderTemplate() {
   return /*html*/ `
-        <div id="header_content" class="content_wrapper p-lr-2r">
+        <div id="header_content" class="content_wrapper p-lr-1r">
             <img class="header_logo" src="./assets/img/Pokedex_Logo_textstudio.png" alt="Pokedex Logo">
         </div>`;
 }
 
 function getPageContentSkeletonTemplate() {
   return /*html*/ `
-    <div id="main_content_container" class="content_wrapper p-lr-2r">
+    <div id="main_content_container" class="content_wrapper p-lr-1r">
       <div id="pokesearch_container">
         <img id="pokesearch_title_img" src="./assets/img/Pokesearch_Logo_textstudio.png" alt="Pokesearch Schriftzug">
         <form id="pokesearch_box">
@@ -53,7 +53,7 @@ function getLoadPokeBtnTemplate() {
 
 function getFooterTemplate() {
   return /*html*/ `
-    <div id="footer_content" class="content_wrapper p-lr-2r">
+    <div id="footer_content" class="content_wrapper p-lr-1r">
         <span>© 2025 Sebastian Hedwig</span>
     </div>`;
 }
